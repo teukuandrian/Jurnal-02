f219ec9996f370331358f736ecc373f46f2fdec7
unknown <teukuandrian127@gmail.com>
2020-09-13 15:56:25 +0700

    HelloWOrld.java

---
+++ New-folder-6-1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    
+	public static void main(String[] args) {
+	    System.out.println("Nama: Teuku Laksamana Andrian");
+	    System.out.println("Nim:6706192090");
+	    System.out.println("Kelas:44-01");
+		}
+}
